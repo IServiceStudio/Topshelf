0f8636360b0393b0e3dcf44fbcfa0551bceca0d2
Feng Lv <75422919+IServiceStudio@users.noreply.github.com>
2021-01-01 20:32:14 +0800

    添加项目文件。

---
+++ TopshelfConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+using Topshelf;
+
+namespace TopshelfConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(c =>
+            {
+                c.Service<Job>(job =>
+                {
+                    job.ConstructUsing(name => new Job());
+                    job.WhenStarted(s=>s.Start());
+                    job.WhenStopped(s => s.Stop());
+                });
+                c.RunAsLocalSystem();
+                c.SetDescription("Sample TopShelf Host");
+                c.SetDisplayName("TopShelf Job");
+                c.SetServiceName("TopShelf Job");
+            });
+        }
+    }
+
+    public class Job
+    {
+        readonly Timer timer;
+        public Job()
+        {
+            timer = new Timer(1000) { AutoReset = true };
+            timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() { timer.Start(); }
+        public void Stop() { timer.Stop(); }
+    }
+}
